b16c644e52893cb7d1cb66b77831c468397b46e9
roy105703007 <roy990427@gmail.com>
2023-07-23 04:06:42 +0200

    deploy on Chiado

---
+++ README.md
@@ -0,0 +1,7 @@
+## Contract Address
+
+| network | contract address                           |
+| :------ | :----------------------------------------- |
+| Celo    | 0x037338701dA162b7fa13d794c2631a5F2DA550D7 |
+| mumbai  | 0x515348dA021B3DEac53DB0Aa39837a7594c9654e |
+| Chiado  | 0x3a8Ee1ec7CA8a517DdDE2b31a9f97a81495fEe8d |
--- script/Deploy/DeployCelo.sh
+++ script/Deploy/DeployCelo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# export CHAIN_ID=59140
-export RPC_URL="https://linea-goerli.infura.io/v3/ae3b481fe2c34ad9a2569ef2f28c29a6"
+# export CHAIN_ID=44787
+export RPC_URL="https://alfajores-forno.celo-testnet.org"
 # export VERIFIER_URL=""
 # export ETHERSCAN_API_KEY=
-export PRIVATE_KEY=0xa93abd947bbc5c79f7c75dab6f0a3ef30478295d93fc4f5cbdfe3d4b56ac5066
+# export PRIVATE_KEY=
 
 forge script script/Lottery2.s.sol \
   --rpc-url=$RPC_URL \
